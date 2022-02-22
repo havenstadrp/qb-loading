f3c49bdfd500227c1bb9064b2a54cf2957840595
bedar89 <bedar89@gmail.com>
2022-02-22 16:46:59 +0000

    few updates with havenstad

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,10 +5,10 @@ description 'qb-loading'
 version '1.0'
 
 lua54 'yes'
-client_script 'client/client.lua'
+-- client_script 'client/client.lua'
 
 files { 'assets/**', 'html/*', }
 
-loadscreen { 'html/index.html' }
+loadscreen 'html/index.html'
 loadscreen_cursor 'yes'
 loadscreen_manual_shutdown 'yes'
